2c3d7706e86eeaf1d89391b218288c8f3892db92
jipengzhu <jipengzhu@qq.com>
2023-09-10 19:21:52 +0800

    Site updated: 2023-09-10 19:21:22

--- downloads/code/java/Solution.java
+++ downloads/code/java/Solution.java
@@ -1,6 +1,40 @@
 public class Solution {
+    public boolean canJump(int[] nums) {
+        return canJump(nums, 0, nums.length - 1);
+    }
+
+    public boolean canJump(int[] nums, int i, int j) {
+        int x = i + nums[i];
+        if (x >= j) {
+            return true;
+        }
+
+        int k = i;
+        while (++k <= x) {
+            if (canJump(nums, k, j)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public static void main(String[] args) {
+        testCanJump1();
+        testCanJump2();
+    }
 
+    public static void testCanJump1() {
+        int[] nums = { 2, 3, 1, 1, 4 };
+
+        boolean canJump = new Solution().canJump(nums);
+        System.out.println(canJump);
+    }
+
+    public static void testCanJump2() {
+        int[] nums = { 3, 2, 1, 0, 4 };
+
+        boolean canJump = new Solution().canJump(nums);
+        System.out.println(canJump);
     }
 }
