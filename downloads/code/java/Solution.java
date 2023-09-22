2cb79aa39dc5020079a2c73f26ef75d43a13f027
jipengzhu <jipengzhu@qq.com>
2023-09-22 19:11:06 +0800

    Site updated: 2023-09-22 19:10:27

--- downloads/code/java/Solution.java
+++ downloads/code/java/Solution.java
@@ -1,57 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int m = matrix.length; // 行数
-        int n = matrix[0].length;// 列数
-        int[] rows = new int[m]; // 存储每一行中第一个遇到的0点的列坐标
-        int[] cols = new int[n]; // 存储每一列中第一个遇到的0点的行坐标
-        for (int i = 0; i < rows.length; i++) {
-            rows[i] = -1;
-        }
-        for (int j = 0; j < cols.length; j++) {
-            cols[j] = -1;
+    public int longestConsecutive(int[] nums) {
+        int max = 0;
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    if (rows[i] == -1) {
-                        rows[i] = j;
-                    }
-                    if (cols[j] == -1) {
-                        cols[j] = i;
-                    }
+        for (Integer num : set) {
+            if (!set.contains(num - 1)) {
+                int end = num + 1;
+                while (set.contains(end)) {
+                    end++;
                 }
 
-                if (rows[i] != -1 || cols[j] != -1) {
-                    matrix[i][j] = 0;
+                int tmp = end - num;
+                if (tmp > max) {
+                    max = tmp;
                 }
             }
         }
 
-        for (int i = 0; i < rows.length; i++) {
-            int v = rows[i];
-            if (v != -1) {
-                // 补齐行数组中每一行的左边部分
-                for (int j = 0; j < v; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        for (int j = 0; j < cols.length; j++) {
-            int v = cols[j];
-            if (v != -1) {
-                // 补齐列数组中每一列的上边部分
-                for (int i = 0; i < v; i++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+        return max;
     }
 
     public static void main(String[] args) {
-        testSetZeroes1();
-        testSetZeroes2();
+        testLongestConsecutive1();
+        testLongestConsecutive2();
     }
 
     public static void printArray(int[] array) {
@@ -66,23 +44,13 @@ class Solution {
         System.out.println(java.util.Arrays.deepToString(arrays));
     }
 
-    public static void testSetZeroes1() {
-        int[][] matrix = {
-                { 1, 1, 1 },
-                { 1, 0, 1 },
-                { 1, 1, 1 }
-        };
-        new Solution().setZeroes(matrix);
-        printArrays(matrix);
+    public static void testLongestConsecutive1() {
+        int[] nums = { 100, 4, 200, 1, 3, 2 };
+        System.out.println(new Solution().longestConsecutive(nums));
     }
 
-    public static void testSetZeroes2() {
-        int[][] matrix = {
-                { 0, 1, 2, 0 },
-                { 3, 4, 5, 2 },
-                { 1, 3, 1, 5 }
-        };
-        new Solution().setZeroes(matrix);
-        printArrays(matrix);
+    public static void testLongestConsecutive2() {
+        int[] nums = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
+        System.out.println(new Solution().longestConsecutive(nums));
     }
 }
