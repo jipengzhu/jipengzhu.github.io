9e43144e5666a93773ae25f54ac2ec7b171ff5aa
jipengzhu <jipengzhu@qq.com>
2023-10-16 18:28:01 +0800

    Site updated: 2023-10-16 18:27:29

--- downloads/code/java/Solution.java
+++ downloads/code/java/Solution.java
@@ -1,136 +1,33 @@
-import java.util.LinkedList;
+public class Solution {
+    public int hIndex(int[] citations) {
+        java.util.Arrays.sort(citations);
 
-class Solution {
-    public String simplifyPath(String path) {
-        int dotCount = 0;
-        LinkedList<Character> stack = new LinkedList<>();
-        char[] charArray = path.toCharArray();
-        for (char c : charArray) {
-            if (c == '.') {
-                dotCount++;
-            } else {
-                if (dotCount > 0) {
-                    if (dotCount == 1 && stack.peek() == '/' && c == '/') {
-                        // do nothing
-                    } else if (dotCount == 2 && stack.peek() == '/' && c == '/') {
-                        if (stack.size() > 1) {
-                            stack.pop();
-
-                            while (stack.size() > 1 && stack.peek() != '/') {
-                                stack.pop();
-                            }
-                        }
-                    } else {
-                        for (int i = 0; i < dotCount; i++) {
-                            stack.push('.');
-                        }
-                    }
-
-                    dotCount = 0;
-                }
-
-                if (c == '/') {
-                    if (stack.isEmpty() || stack.peek() != c) {
-                        stack.push(c);
-                    }
-                } else {
-                    stack.push(c);
-                }
-            }
+        int h = 0;
+        int i = citations.length - 1;
+        while (i >= 0 && citations[i] > h) {
+            h++;
+            i--;
         }
 
-        if (dotCount > 0) {
-            if (dotCount == 1 && stack.peek() == '/') {
-                // do nothing
-            } else if (dotCount == 2 && stack.peek() == '/') {
-                if (stack.size() > 1) {
-                    stack.pop();
-
-                    while (stack.size() > 1 && stack.peek() != '/') {
-                        stack.pop();
-                    }
-                }
-            } else {
-                for (int i = 0; i < dotCount; i++) {
-                    stack.push('.');
-                }
-            }
-
-            dotCount = 0;
-        }
-
-        while (stack.size() > 1 && stack.peek() == '/') {
-            stack.pop();
-        }
-
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.insert(0, stack.pop());
-        }
-
-        return sb.toString();
+        return h;
     }
 
     public static void main(String[] args) {
-        testSimplifyPath1();
-        testSimplifyPath2();
-        testSimplifyPath3();
-        testSimplifyPath4();
-        testSimplifyPath5();
-        testSimplifyPath6();
-        testSimplifyPath7();
-        testSimplifyPath8();
-        testSimplifyPath9();
-        testSimplifyPath10();
+        testCase1();
+        testCase2();
     }
 
-    public static void testSimplifyPath1() {
-        String s = "/home/";
-        System.out.println((new Solution().simplifyPath(s)));
+    public static void testCase1() {
+        int[] nums = { 3, 0, 6, 1, 5 };
+
+        int result = new Solution().hIndex(nums);
+        System.out.println(result);
     }
 
-    public static void testSimplifyPath2() {
-        String s = "/../";
-        System.out.println((new Solution().simplifyPath(s)));
-    }
+    public static void testCase2() {
+        int[] nums = { 1, 3, 1 };
 
-    public static void testSimplifyPath3() {
-        String s = "/home//foo/";
-        System.out.println((new Solution().simplifyPath(s)));
-    }
-
-    public static void testSimplifyPath4() {
-        String s = "/a/./b/../../c/";
-        System.out.println((new Solution().simplifyPath(s)));
-    }
-
-    public static void testSimplifyPath5() {
-        String s = "/...";
-        System.out.println((new Solution().simplifyPath(s)));
-    }
-
-    public static void testSimplifyPath6() {
-        String s = "/..";
-        System.out.println((new Solution().simplifyPath(s)));
-    }
-
-    public static void testSimplifyPath7() {
-        String s = "/..hidden";
-        System.out.println((new Solution().simplifyPath(s)));
-    }
-
-    public static void testSimplifyPath8() {
-        String s = "/.hidden";
-        System.out.println((new Solution().simplifyPath(s)));
-    }
-
-    public static void testSimplifyPath9() {
-        String s = "/hello../world";
-        System.out.println((new Solution().simplifyPath(s)));
-    }
-
-    public static void testSimplifyPath10() {
-        String s = "/hello./world";
-        System.out.println((new Solution().simplifyPath(s)));
+        int result = new Solution().hIndex(nums);
+        System.out.println(result);
     }
 }
--- downloads/code/java/Test.java
+++ downloads/code/java/Test.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -13,4 +15,61 @@ public class Test {
         s = s.replace("}}", "}\n}");
         System.out.println(s);
     }
+
+    public static class ArrayUtils {
+        public static void printArray(int[] array) {
+            System.out.println(java.util.Arrays.toString(array));
+        }
+
+        public static void printArray(int[] array, int len) {
+            System.out.println(java.util.Arrays.toString(java.util.Arrays.copyOf(array, len)));
+        }
+
+        public static void printArrays(int[][] arrays) {
+            System.out.println(java.util.Arrays.deepToString(arrays));
+        }
+    }
+
+    public static class ListUtils {
+        public static int[] listToArray(List<Integer> list) {
+            if (list == null || list.isEmpty()) {
+                return new int[0];
+            }
+
+            int[] array = new int[list.size()];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = list.get(i);
+            }
+
+            return array;
+        }
+
+        public static int[][] listsToArrays(List<List<Integer>> lists) {
+            if (lists == null || lists.isEmpty()) {
+                return new int[0][0];
+            }
+
+            int row = lists.size();
+            int col = lists.get(0).size();
+            int[][] arrays = new int[row][col];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    arrays[i][j] = lists.get(i).get(j);
+                }
+            }
+
+            return arrays;
+        }
+    }
+
+    int result;
+    int expect;
+
+    // String sp1 = "^\s{1,}print.*|System.out.println\((?!(java.util|expect|result))";
+    // String sp2 = "ArrayUtils.print.*\((?!(expect|result))|System.out.println\((?!(java.util|expect|result))";
+    // String sp3 = "public.*To[al].*\(";
+    // String sc = "ArrayUtils.print|System.out.println\((?!java.util)";
+
+    // String tp1 = "test(?!Case).*\d\("
+    // String tc = "testCase\d\("
 }
