d08d7f57e11746c41f349f8d20d6ada1be5d5764
jipengzhu <jipengzhu@qq.com>
2023-10-23 20:21:27 +0800

    Site updated: 2023-10-23 20:20:32

--- downloads/code/java/Solution.java
+++ downloads/code/java/Solution.java
@@ -1,136 +1,69 @@
 public class Solution {
-    public ListNode partition(ListNode head, int x) {
-        ListNode l1 = new ListNode(0);
-        ListNode l2 = new ListNode(0);
-
-        ListNode t1 = l1;
-        ListNode t2 = l2;
-
-        ListNode p = head;
-        while (p != null) {
-            if (p.val < x) {
-                t1.next = p;
-                t1 = t1.next;
-            } else {
-                t2.next = p;
-                t2 = t2.next;
-            }
-
-            p = p.next;
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
         }
 
-        // 清空原有的关系
-        t1.next = null;
-        t2.next = null;
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
 
-        t1.next = l2.next;
-        head = l1.next;
+        int k = l;
+        if (r > l) {
+            k = r;
+        }
 
-        return head;
+        return k + 1;
     }
 
-    static class ListNode {
+    static public class TreeNode {
         int val;
-        ListNode next;
+        TreeNode left;
+        TreeNode right;
 
-        ListNode(int x) {
-            val = x;
-            next = null;
+        TreeNode() {
         }
 
-        public static ListNode genLinkedList(int[] nums, boolean withHead, int loopPos) {
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public static TreeNode genTree(Integer[] nums) {
             if (nums.length == 0) {
                 return null;
             }
 
-            int val = withHead ? Integer.MIN_VALUE : nums[0];
-            ListNode head = new ListNode(val);
+            java.util.Queue<TreeNode> queue = new java.util.LinkedList<>();
+            TreeNode root = new TreeNode(nums[0]);
+            queue.offer(root);
 
-            ListNode loop = withHead ? null : loopPos == 0 ? head : null;
+            int i = 1;
+            while (i < nums.length) {
+                TreeNode node = queue.poll();
 
-            int b = withHead ? 0 : 1;
-            ListNode p = head;
-            for (int i = b; i < nums.length; i++) {
-                p.next = new ListNode(nums[i]);
-                p = p.next;
+                Integer leftVal = nums[i++];
+                if (leftVal != null) {
+                    TreeNode leftNode = new TreeNode(leftVal);
+                    node.left = leftNode;
 
-                if (loopPos == i) {
-                    loop = p;
+                    queue.offer(leftNode);
+                }
+
+                Integer rightVal = nums[i++];
+                if (rightVal != null) {
+                    TreeNode rightNode = new TreeNode(rightVal);
+                    node.right = rightNode;
+
+                    queue.offer(rightNode);
                 }
             }
 
-            if (loop != null) {
-                p.next = loop;
-            }
-
-            return head;
-        }
-
-        public static ListNode genLinkedListWithHeadNode(int[] nums) {
-            return genLinkedList(nums, true, -1);
-        }
-
-        public static ListNode genLinkedListWithoutHeadNode(int[] nums) {
-            return genLinkedList(nums, false, -1);
-        }
-
-        public static ListNode genLoopedLinkedListWithHeadNode(int[] nums, int pos) {
-            return genLinkedList(nums, true, pos);
-        }
-
-        public static ListNode genLoopedLinkedListWithoutHeadNode(int[] nums, int pos) {
-            return genLinkedList(nums, false, pos);
-        }
-
-        public static int getCount(ListNode head, boolean withHead) {
-            ListNode p = withHead ? head.next : head;
-
-            int c = 0;
-            while (p != null) {
-                c++;
-                p = p.next;
-            }
-
-            return c;
-        }
-
-        public static int[] toArray(ListNode head, boolean withHead) {
-            int c = getCount(head, withHead);
-            int[] nums = new int[c];
-
-            ListNode p = withHead ? head.next : head;
-
-            int k = 0;
-            while (p != null) {
-                nums[k] = p.val;
-
-                k++;
-                p = p.next;
-            }
-
-            return nums;
-        }
-
-        public static int[] toArrayWithHeadNode(ListNode head) {
-            return toArray(head, true);
-        }
-
-        public static int[] toArrayWithoutHeadNode(ListNode head) {
-            return toArray(head, false);
-        }
-    }
-
-    public static class ArrayUtils {
-        public static void printArray(int[] array) {
-            System.out.println(java.util.Arrays.toString(array));
-        }
-
-        public static void printArray(int[] array, int len) {
-            System.out.println(java.util.Arrays.toString(java.util.Arrays.copyOf(array, len)));
-        }
-
-        public static void printArrays(int[][] arrays) {
-            System.out.println(java.util.Arrays.deepToString(arrays));
+            return root;
         }
     }
 
@@ -140,20 +73,18 @@ public class Solution {
     }
 
     public static void testCase1() {
-        int[] nums = { 1, 4, 3, 2, 5, 2 };
-        int x = 3;
+        Integer[] nums = { 3, 9, 20, null, null, 15, 7 };
 
-        ListNode link = ListNode.genLinkedListWithoutHeadNode(nums);
-        ListNode result = new Solution().partition(link, x);
-        ArrayUtils.printArray(ListNode.toArrayWithoutHeadNode(result));
+        TreeNode tree = TreeNode.genTree(nums);
+        int result = new Solution().maxDepth(tree);
+        System.out.println(result);
     }
 
     public static void testCase2() {
-        int[] nums = { 2, 1 };
-        int x = 2;
+        Integer[] nums = { 1, null, 2 };
 
-        ListNode link = ListNode.genLinkedListWithoutHeadNode(nums);
-        ListNode result = new Solution().partition(link, x);
-        ArrayUtils.printArray(ListNode.toArrayWithoutHeadNode(result));
+        TreeNode tree = TreeNode.genTree(nums);
+        int result = new Solution().maxDepth(tree);
+        System.out.println(result);
     }
 }
