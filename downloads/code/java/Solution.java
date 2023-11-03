b897145d9bade08caf037744faff1b14ac25e018
jipengzhu <jipengzhu@qq.com>
2023-11-03 15:27:50 +0800

    Site updated: 2023-11-03 15:26:13

--- downloads/code/java/Solution.java
+++ downloads/code/java/Solution.java
@@ -1,55 +1,17 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Queue;
 
 public class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<Integer> postorderTraversal(TreeNode root) {
         if (root == null) {
             return new LinkedList<>();
         }
 
-        List<List<Integer>> list = new LinkedList<>();
+        List<Integer> list = new LinkedList<>();
 
-        int curLevel = 1;
-        List<Integer> values = new LinkedList<>();
-
-        Map<TreeNode, Integer> map = new HashMap<>(); // 存储每个结点的层次
-        map.put(root, 1);
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-
-            if (node.left != null) {
-                queue.offer(node.left);
-
-                map.put(node.left, map.get(node) + 1);
-            }
-
-            if (node.right != null) {
-                queue.offer(node.right);
-
-                map.put(node.right, map.get(node) + 1);
-            }
-
-            if (map.get(node) == curLevel) {
-                values.add(node.val);
-            } else {
-                list.add(values);
-
-                values = new ArrayList<>();
-                values.add(node.val);
-            }
-
-            curLevel = map.get(node);
-        }
-
-        list.add(values);
+        list.addAll(postorderTraversal(root.left));
+        list.addAll(postorderTraversal(root.right));
+        list.add(root.val);
 
         return list;
     }
@@ -142,29 +104,43 @@ public class Solution {
     }
 
     public static class ListUtils {
-        public static void printList(List<Object> list) {
+        public static void printList(java.util.List<? extends Object> list) {
             System.out.println(java.util.Arrays.toString(list.toArray(new Object[0])));
         }
 
-        public static void printLists1(List<List<Integer>> lists) {
+        public static void printLines(java.util.List<? extends Object> list) {
+            if (list == null) {
+                return;
+            }
+
+            for (Object o : list) {
+                System.out.println(o);
+            }
+            System.out.println();
+        }
+
+        public static void printLists1(java.util.List<java.util.List<Integer>> lists) {
             Object[] arrays = new Object[lists.size()];
             for (int i = 0; i < lists.size(); i++) {
-
-                List<Integer> list = lists.get(i);
-                arrays[i] = list.toArray(new Object[0]);
-
+                arrays[i] = lists.get(i).toArray(new Object[0]);
             }
 
             System.out.println(java.util.Arrays.deepToString(arrays));
         }
 
-        public static void printLists2(List<List<Double>> lists) {
+        public static void printLists2(java.util.List<java.util.List<Double>> lists) {
             Object[] arrays = new Object[lists.size()];
             for (int i = 0; i < lists.size(); i++) {
+                arrays[i] = lists.get(i).toArray(new Object[0]);
+            }
 
-                List<Double> list = lists.get(i);
-                arrays[i] = list.toArray(new Object[0]);
+            System.out.println(java.util.Arrays.deepToString(arrays));
+        }
 
+        public static void printLists3(java.util.List<java.util.List<String>> lists) {
+            Object[] arrays = new Object[lists.size()];
+            for (int i = 0; i < lists.size(); i++) {
+                arrays[i] = lists.get(i).toArray(new Object[0]);
             }
 
             System.out.println(java.util.Arrays.deepToString(arrays));
@@ -178,26 +154,26 @@ public class Solution {
     }
 
     public static void testCase1() {
-        Integer[] nums = { 3, 9, 20, null, null, 15, 7 };
+        Integer[] nums = { 1, null, 2, 3 };
 
         TreeNode tree = TreeNode.genTree(nums);
-        List<List<Integer>> result = new Solution().levelOrder(tree);
-        ListUtils.printLists1(result);
+        List<Integer> result = new Solution().postorderTraversal(tree);
+        ListUtils.printList(result);
     }
 
     public static void testCase2() {
-        Integer[] nums = { 1 };
-
-        TreeNode tree = TreeNode.genTree(nums);
-        List<List<Integer>> result = new Solution().levelOrder(tree);
-        ListUtils.printLists1(result);
-    }
-
-    public static void testCase3() {
         Integer[] nums = {};
 
         TreeNode tree = TreeNode.genTree(nums);
-        List<List<Integer>> result = new Solution().levelOrder(tree);
-        ListUtils.printLists1(result);
+        List<Integer> result = new Solution().postorderTraversal(tree);
+        ListUtils.printList(result);
+    }
+
+    public static void testCase3() {
+        Integer[] nums = { 1 };
+
+        TreeNode tree = TreeNode.genTree(nums);
+        List<Integer> result = new Solution().postorderTraversal(tree);
+        ListUtils.printList(result);
     }
 }
