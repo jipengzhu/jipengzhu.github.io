317cd1186b92c84e4ad7dffb69e2c5f163cafaf4
jipengzhu <jipengzhu@qq.com>
2023-11-09 18:31:20 +0800

    Site updated: 2023-11-09 18:30:33

--- downloads/code/java/Solution.java
+++ downloads/code/java/Solution.java
@@ -1,104 +1,32 @@
 public class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null) {
-            return false;
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
         }
 
-        if (root.left == null && root.right == null) {
-            if (targetSum == root.val) {
-                return true;
+        // 本质上是求多个上升区间的区间差之和（股票的本质是低买高卖）
+
+        int maxProfit = 0;
+        int minPrice = prices[0]; // 记录上升区间的最小值
+        int maxPrice = minPrice; // 记录上升区间的最大值
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] >= maxPrice) {
+                // 还是上升趋势，继续扩大区间
+                maxPrice = prices[i];
+            } else {
+                // 出现拐点了
+                maxProfit = maxProfit + (maxPrice - minPrice);
+
+                // 重设窗口位置
+                minPrice = prices[i];
+                maxPrice = minPrice;
             }
         }
 
-        return hasPathSum(root.left, targetSum - root.val) &&
-                hasPathSum(root.right, targetSum - root.val);
-    }
+        maxProfit = maxProfit + (maxPrice - minPrice);
 
-    static public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode genTree(Integer[] nums) {
-            if (nums.length == 0) {
-                return null;
-            }
-
-            java.util.Queue<TreeNode> queue = new java.util.LinkedList<>();
-            TreeNode root = new TreeNode(nums[0]);
-            queue.offer(root);
-
-            int i = 1;
-            while (i < nums.length) {
-                TreeNode node = queue.poll();
-
-                Integer leftVal = i < nums.length ? nums[i++] : null;
-                if (leftVal != null) {
-                    TreeNode leftNode = new TreeNode(leftVal);
-                    node.left = leftNode;
-
-                    queue.offer(leftNode);
-                }
-
-                Integer rightVal = i < nums.length ? nums[i++] : null;
-                if (rightVal != null) {
-                    TreeNode rightNode = new TreeNode(rightVal);
-                    node.right = rightNode;
-
-                    queue.offer(rightNode);
-                }
-            }
-
-            return root;
-        }
-
-        public static Integer[] toArray(TreeNode root) {
-            if (root == null) {
-                return new Integer[0];
-            }
-
-            java.util.LinkedList<Integer> list = new java.util.LinkedList<>();
-            list.add(root.val);
-
-            java.util.Queue<TreeNode> queue = new java.util.LinkedList<>();
-            queue.offer(root);
-
-            while (!queue.isEmpty()) {
-                TreeNode node = queue.poll();
-                TreeNode left = node.left;
-                TreeNode right = node.right;
-
-                list.add(left != null ? left.val : null);
-                list.add(right != null ? right.val : null);
-
-                if (left != null) {
-                    queue.offer(left);
-                }
-
-                if (right != null) {
-                    queue.offer(right);
-                }
-            }
-
-            while (!list.isEmpty() && list.peekLast() == null) {
-                list.pollLast();
-            }
-
-            return list.toArray(new Integer[0]);
-        }
+        return maxProfit;
     }
 
     public static void main(String[] args) {
@@ -108,29 +36,23 @@ public class Solution {
     }
 
     public static void testCase1() {
-        Integer[] nums = { 5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1 };
-        int sum = 22;
+        int[] nums = { 7, 1, 5, 3, 6, 4 };
 
-        TreeNode tree = TreeNode.genTree(nums);
-        boolean result = new Solution().hasPathSum(tree, sum);
+        int result = new Solution().maxProfit(nums);
         System.out.println(result);
     }
 
     public static void testCase2() {
-        Integer[] nums = { 1, 2, 3 };
-        int sum = 5;
+        int[] nums = { 1, 2, 3, 4, 5 };
 
-        TreeNode tree = TreeNode.genTree(nums);
-        boolean result = new Solution().hasPathSum(tree, sum);
+        int result = new Solution().maxProfit(nums);
         System.out.println(result);
     }
 
     public static void testCase3() {
-        Integer[] nums = {};
-        int sum = 0;
+        int[] nums = { 7, 6, 4, 3, 1 };
 
-        TreeNode tree = TreeNode.genTree(nums);
-        boolean result = new Solution().hasPathSum(tree, sum);
+        int result = new Solution().maxProfit(nums);
         System.out.println(result);
     }
 }
